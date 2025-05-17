41405facc48858f864a34c71d732226aff0c0bce
Francisco Altimari <francisco@altimari.nl>
2025-05-16 21:03:13 -0300

    Add disable-key-capture script

---
+++ src/disable-key-capture.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name           Disable key capture
+// @description    Stop websites from highjacking keyboard shortcuts
+// @version        0.0.0
+//
+// @run-at         document-start
+// @include        *
+// @exclude        *.google.*
+// @grant          none
+// ==/UserScript==
+
+const keys = ['s', 't', 'w', 'l'];
+
+// isTrusted -> userAgent
+// metaKey, ctrlKey, shiftKey, altKey,
+
+document.addEventListener('keydown', event => {
+
+    if (
+        (keys.indexOf(event.key) !== -1 && event.metaKey)
+        || event.key === 'Escape'
+    ) {
+        event.cancelBubble = true;
+        event.stopImmediatePropagation();
+
+        console.log(`Prevent capture of "${event.key}", gotcha!`);
+    }
+
+    return false;
+
+}, true);
+
+document.addEventListener('click', event => {
+    if (event.metaKey) {
+        event.cancelBubble = true;
+        event.stopImmediatePropagation();
+
+        console.log(`Prevent capture of Command click, gotcha!`);
+    }
+
+}, true);
